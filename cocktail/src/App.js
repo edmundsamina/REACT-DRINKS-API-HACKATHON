7880523fd0c716594f10c1fe0e8480ee542c4543
Tirsog <tirsog@gmail.com>
2022-11-18 12:30:56 +0000

    Some stuff work

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
 import Button from './components/button';
 import Card from './components/card';
-
-
-async function getDrink(){
-  const responseJSON = await fetch("https://www.boredapi.com/api/activity")
-  const response = await responseJSON.json();
-  console.log(response)
-  console.log("clicked")
-}
-
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  function handleClick(){
-    getDrink()
-  }
+/*   useEffect(()=>{
+getDrink()
+  },[]
+  ) */
+  
+const [drinkName, setDrinkName] = useState("Potato")
+const [drinkImg, setDrinkImg] = useState("")
+
+
+async function getDrink(){
+const responseJSON = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+const response = await responseJSON.json();
+console.log(response)
+setDrinkName(response.drinks[0].strDrink)
+setDrinkImg(response.drinks[0].strDrinkThumb)
+console.log(drinkName)
+}
+
 
   return (
     <div className="App">
     <h1>The cocktail App</h1>
     <p>Read about the best cocktails in the world:</p>
-      <Button onClick= {handleClick} text="New Cocktail!"/>
-      <Card />
+      <Button text="New Cocktail!" onClick={()=>{getDrink()}}/>
+      <Card drinkName={drinkName} drinkImg={drinkImg}/>
     </div>
   );
 }
--- cocktail/src/components/card/index.js
+++ cocktail/src/components/card/index.js
@@ -1,23 +1,11 @@
 import React from 'react'
-// import { useEffect, useState } from 'react';
 
 export default function Card(props) {
-//   // // const [drinkName, setDrinkName] = useState("")
 
-//   // useEffect(()=>{
-//   // async function getDrink(){
-//   //   const responseJSON = await fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
-//   //   const response = await responseJSON.json();
-//   //   console.log(response)
-//   //   // setDrinkName(response.drink[0].strDrink)
-//   //   // console.log(drinkName)
-//   // }
-//   // getDrink();
-// })
   return (
     <div>
-    <h2>{}</h2>
-    <img src={props.image} alt={props.alt}/>
+    <h2>{props.drinkName}</h2>
+    <img src={props.drinkImg} alt={props.drinkName}/>
     </div>
   )
 }
