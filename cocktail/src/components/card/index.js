d31a08e6969c1b107a9e9a021a5d4107ba0b1773
Tirsog <tirsog@gmail.com>
2022-11-18 15:35:55 +0000

    Minor changes in design

--- cocktail/src/components/card/index.js
+++ cocktail/src/components/card/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './styles.css';
 export default function Card(props) {
 
   return (
     <div className = {props.className}>
-    <h3>{props.drinkName}</h3>
-    <img src={props.drinkImg} alt={props.drinkName}/>
+    <h3 className='header'>{props.drinkName}</h3>
+    <img className='img' src={props.drinkImg} alt={props.drinkName}/>
     <p>{props.strInstructions}</p>
     </div>
   )
