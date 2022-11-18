213ab67a81dd5e46b6f3b6952942f9610803bfc1
edmundsamina <edmundsamina@gmail.com>
2022-11-18 15:02:25 +0000

    some css done

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -4,12 +4,27 @@ import Card from "./components/card";
 import Input from "./components/input";
 import { useState } from "react";
 
-
 function App() {
   const [drinkName, setDrinkName] = useState();
   const [drinkImg, setDrinkImg] = useState();
   const [instructions, setInstructions] = useState();
-  
+  const [drinkByName, setDrinkByName] = useState("");
+  const [drinkCard, setDrinkCard] = useState({});
+
+  function inputValue(e) {
+    e.preventDefault();
+    let name = e.target.value;
+    setDrinkByName(name);
+  }
+
+  async function getDrinkByName() {
+    const responseJSON = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkByName}`
+    );
+    const response = await responseJSON.json();
+    setDrinkCard(response.drinks[0]);
+    console.log(response.drinks[0]);
+  }
 
   async function getDrink() {
     const responseJSON = await fetch(
@@ -23,20 +38,30 @@ function App() {
     console.log(drinkName);
   }
 
-  
-
   return (
     <div className="App">
       <h1>🤖AI BARETENDER🤖</h1>
       <h2 className="sub-heading">
         let our robot bartender pick a drink for you!
       </h2>
-      <Button
+      <div className="buttonContainer">
+        <Button
           text="Random Cocktail!"
           onClick={() => {
             getDrink();
           }}
         />
+        <div className="inputButton">
+          <Input onChange={inputValue} />
+          <Button
+            text="Get Cocktail"
+            className="byNameButton"
+            onClick={() => {
+              getDrinkByName();
+            }}
+          />
+        </div>
+      </div>
       <div className="cardContainer">
         <Card
           className="randomDrink"
@@ -44,9 +69,13 @@ function App() {
           drinkImg={drinkImg}
           strInstructions={instructions}
         />
-        <Input/>
-         
 
+        <Card
+          className="newDrink"
+          drinkName={drinkCard.strDrink}
+          drinkImg={drinkCard.strDrinkThumb}
+          strInstructions={drinkCard.strInstructions}
+        />
       </div>
     </div>
   );
