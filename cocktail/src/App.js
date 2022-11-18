9c4643ddeec5ed12a6cb08b68ff3233d162a2b50
edmundsamina <edmundsamina@gmail.com>
2022-11-18 11:23:50 +0000

    nothing done

--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './components/button';
 import Card from './components/card';
-import { useEffect, useState } from 'react';
+
+
+async function getDrink(){
+  const responseJSON = await fetch("https://www.boredapi.com/api/activity")
+  const response = await responseJSON.json();
+  console.log(response)
+  console.log("clicked")
+}
+
 
 function App() {
-const [drink, setDrink] = useState()
 
-  useEffect(()=>{
-  async function getDrink(){
-    const responseJSON = await fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
-    const response = await responseJSON.json();
-    setDrink(response.drink[0])
+  function handleClick(){
+    getDrink()
   }
-  getDrink();
-})
-
 
   return (
     <div className="App">
     <h1>The cocktail App</h1>
     <p>Read about the best cocktails in the world:</p>
-      <Button text="New Cocktail!"/>
-      <Card name={drink.strDrink}/>
+      <Button onClick= {handleClick} text="New Cocktail!"/>
+      <Card />
     </div>
   );
 }
--- cocktail/src/components/card/index.js
+++ cocktail/src/components/card/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+// import { useEffect, useState } from 'react';
 
-export default function card(props) {
+export default function Card(props) {
+//   // // const [drinkName, setDrinkName] = useState("")
+
+//   // useEffect(()=>{
+//   // async function getDrink(){
+//   //   const responseJSON = await fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
+//   //   const response = await responseJSON.json();
+//   //   console.log(response)
+//   //   // setDrinkName(response.drink[0].strDrink)
+//   //   // console.log(drinkName)
+//   // }
+//   // getDrink();
+// })
   return (
     <div>
-    <h2>{props.name}</h2>
+    <h2>{}</h2>
     <img src={props.image} alt={props.alt}/>
     </div>
   )
